95588faec0d9febe748291414106ff50d5f191b8
rezan83 <rezan.moh83@gmail.com>
2018-10-16 20:04:16 +0200

    fixing git bug

---
+++ js/work.js
@@ -0,0 +1,27 @@
+var works = [
+  {
+    title: "rails e-commerce",
+    pic: "img/bike1.jpg",
+    link: "https://rezanbikes.herokuapp.com/"
+  },
+  {
+    title: "GitStalker Github api",
+    pic: "img/tiger1.jpg",
+    link: "gitstalker.html"
+  },
+  {
+    title: "MyWeather",
+    pic: "img/weather.jpg",
+    link: "MyWeather.html"
+  },
+  {
+    title: "rails Twitter clone",
+    pic: "img/twitterclone.jpg",
+    link: "https://omrailstut.herokuapp.com/"
+  },
+  {
+    title: "Twitter front-end clone",
+    pic: "img/twitterclone.jpg",
+    link: "tclone.html"
+  }
+];
